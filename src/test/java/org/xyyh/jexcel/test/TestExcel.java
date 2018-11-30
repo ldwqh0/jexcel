a28ff97dbf7f2f42711efe5a2c6d6d5765de8864
liganggang <liganggang@jusfoun.com>
2018-11-30 14:53:18 +0800

    test

--- src/test/java/org/xyyh/jexcel/test/TestExcel.java
+++ src/test/java/org/xyyh/jexcel/test/TestExcel.java
@@ -3,7 +3,10 @@ package org.xyyh.jexcel.test;
 /**
  * @author: max
  * @Date: 2018/11/30 0030 11:45
- * @Description:
+ * @Description: 测试提交
  */
 public class TestExcel {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
